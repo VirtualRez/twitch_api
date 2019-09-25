95f0044148174469120d54457ca3377d7516ea89
alfredocanales <a.canales@hotmail.es>
2019-09-25 12:25:09 +0200

    table OK

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ $.ajax({//GET Top 10 Streamers by number of views
     'url': `https://api.twitch.tv/kraken/streams/?limit=10`,
     'headers': { 'Client-ID': 'e58bb9u3nmtqp4ikxaz1qg05kelpy7l', 'Accept': 'application/vnd.twitchtv.v5+json' },
     'success': (data) => {
-        data.streams.forEach((element, index) => {
+        console.log(data);
+        data.streams.forEach(element => {
+            
             top1.append(
             `<tr>
-                <th>${index+1}</th>
+                <th><img class="logoReduction" src="${element.channel.logo}"></img></th>
                 <td class="nameCap">${element.channel.name}</td>
+                <td>${element.channel.followers}</td>
+                <td>${element.viewers}</td>
+                <td><a href="${element.channel.url}" target="_blank">${element.channel.name}.twitch</a></td>
             </tr>`);
         })
     }
