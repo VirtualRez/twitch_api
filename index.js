e4dcbe0a5ca93fe46cbdb3d3487f44cd46c920d2
alfredocanales <a.canales@hotmail.es>
2019-09-25 11:37:48 +0200

    petición get OK

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var top1 = $("#t1");
+
+$.ajax({//GET Top 10 Streamers by number of views
+    'url': `https://api.twitch.tv/kraken/streams/?limit=10`,
+    'headers': { 'Client-ID': 'e58bb9u3nmtqp4ikxaz1qg05kelpy7l', 'Accept': 'application/vnd.twitchtv.v5+json' },
+    'success': (data) => {
+        data.streams.forEach((element, index) => {
+            top1.append(
+            `<tr>
+                <th>${index+1}</th>
+                <td class="nameCap">${element.channel.name}</td>
+            </tr>`);
+        })
+    }
+});
