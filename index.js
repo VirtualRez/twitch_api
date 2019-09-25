db15d2ef932639c152c013be8d3671ab99339726
alfredocanales <a.canales@hotmail.es>
2019-09-25 13:27:58 +0200

    added style on table

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ $.ajax({//GET Top 10 Streamers by number of views
     'headers': { 'Client-ID': 'e58bb9u3nmtqp4ikxaz1qg05kelpy7l', 'Accept': 'application/vnd.twitchtv.v5+json' },
     'success': (data) => {
         console.log(data);
-        data.streams.forEach(element => {
+        data.streams.forEach((element,index) => {
             
             top1.append(
             `<tr>
-                <th><img class="logoReduction" src="${element.channel.logo}"></img></th>
+                <th id="a${index}i"><img class="logoReduction" src="${element.channel.logo}"></img></th>
                 <td class="nameCap">${element.channel.name}</td>
                 <td>${element.channel.followers}</td>
                 <td>${element.viewers}</td>
-                <td><a href="${element.channel.url}" target="_blank">${element.channel.name}.twitch</a></td>
+                <td id="a${index}f"><a href="${element.channel.url}" target="_blank">${element.channel.name}.twitch</a></td>
             </tr>`);
         })
     }
